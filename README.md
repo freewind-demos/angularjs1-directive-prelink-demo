68e19e470dba923489c4c66b8d9aa4447771369d
freewind <nowindlee@gmail.com>
2018-08-31 22:39:53 +0800

    finally have a good demo of prelink

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 AngularJS1 Directive PreLink Demo
 =================================
 
-Do some DOM operations in `link` function of directive.
+`preLink`在Angular中用的并不多，因为大多数情况下，我们使用`link`（即`postLink`）就已经足够；
+就算有一些代码需要写在`preLink`中，实际上也可以写在`controller`中。
+
+想破了头，总算想到一种情况：在一个directive中，需要根据其它的directive做一些初始化工作，方便自己的子directive，这时只能在`preLink`中实现（结合`require`），
+因为在别的方法中，要么拿不到（比如`controller`），要么执行时机不对（比如`link`），`preLink`就是最好的选择。
 
 ```
 npm install
--- app.js
+++ app.js
@@ -1,9 +1,37 @@
-const app = angular.module('app', []);
+const app = angular.module('app', [])
+
 app.directive('bgColor', function () {
     return {
-        restrict: 'A',
-        link: function (scope, element, attrs) {
-            element.css("background-color", attrs.bgColor)
+        controller: function ($scope, $element, $attrs) {
+            this.bgColor = $attrs.bgColor
         }
     }
-});
+})
+
+app.directive('container', function () {
+    return {
+        restrict: 'E',
+        require: ['?bgColor'],
+        link: {
+            pre: function preLink($scope, $element, $attrs, ctrls) {
+                const bgColorCtrl = ctrls[0]
+                $scope.config = {
+                    bgColor: bgColorCtrl && bgColorCtrl.bgColor
+                }
+            }
+        }
+    }
+})
+
+app.directive('row', function () {
+    return {
+        restrict: 'A',
+        template: '<div>Hello, PreLink</div>',
+        link: function ($scope, $element) {
+            const bgColor = $scope.config.bgColor
+            if (bgColor) {
+                $element.css('background-color', bgColor)
+            }
+        }
+    }
+})
